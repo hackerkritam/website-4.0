467d811f73cfb2848153b03910fc7729efde6a2a
Sujan Ghimire <ghimire.amrit147@gmail.com>
2023-12-23 17:05:56 +0545

    changed getServerSideProps to getStaticProps

--- src/pages/gallery/[slug]/[subslug].js
+++ src/pages/gallery/[slug]/[subslug].js
@@ -1,23 +1,23 @@
-import Image from "next/image"
-import Link from "next/link"
-import { useRouter } from "next/router"
-import React, { useEffect, useState } from "react"
-import { AnimatePresence, motion } from "framer-motion"
-import Skeleton from "react-loading-skeleton"
-import "react-loading-skeleton/dist/skeleton.css"
-import { Poppins, Montserrat } from "next/font/google"
-import useScrollBlock from "../../../../customHooks/useScrollBlock"
-import fs from "fs"
-import path from "path"
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Poppins, Montserrat } from 'next/font/google';
+import useScrollBlock from '../../../../customHooks/useScrollBlock';
+import fs from 'fs';
+import path from 'path';
 
 const poppins = Poppins({
-  weight: ["400", "200", "100", "300", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-})
+  weight: ['400', '200', '100', '300', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
 const montserrat = Montserrat({
-  weight: ["400", "200", "100", "300", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-})
+  weight: ['400', '200', '100', '300', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
 
 // const GallerysubslugSlug = () => {
 //   const router = useRouter();
@@ -224,41 +224,41 @@ const montserrat = Montserrat({
 // export default GallerysubslugSlug;
 
 const GallerysubslugSlug = ({ imageNames }) => {
-  const [loading, setLoading] = useState()
-  const [popupIndex, setPopupIndex] = useState()
-  const [blockScroll, allowScroll] = useScrollBlock()
-  const [selectedId, setSelectedId] = useState()
+  const [loading, setLoading] = useState();
+  const [popupIndex, setPopupIndex] = useState();
+  const [blockScroll, allowScroll] = useScrollBlock();
+  const [selectedId, setSelectedId] = useState();
 
-  const router = useRouter()
-  const slug = router.query.slug
-  const subslug = router.query.subslug
+  const router = useRouter();
+  const slug = router.query.slug;
+  const subslug = router.query.subslug;
 
-  imageNames = imageNames.filter((str) => !str.includes("thumbnail"))
+  imageNames = imageNames.filter((str) => !str.includes('thumbnail'));
 
   const variants = {
     hidden: { opacity: 0, y: 40 },
     visible: { opacity: 1, y: 0 },
-  }
+  };
 
   const showImagePopup = (index) => {
-    setPopupIndex(index)
-    blockScroll()
-  }
+    setPopupIndex(index);
+    blockScroll();
+  };
 
   const hideImagePopup = () => {
-    setPopupIndex(null)
-    allowScroll()
-  }
+    setPopupIndex(null);
+    allowScroll();
+  };
 
   const showNextImage = () => {
-    setPopupIndex((prevIndex) => (prevIndex + 1) % imageNames.length)
-  }
+    setPopupIndex((prevIndex) => (prevIndex + 1) % imageNames.length);
+  };
 
   const showPrevImage = () => {
     setPopupIndex((prevIndex) =>
       prevIndex === 0 ? imageNames.length - 1 : prevIndex - 1
-    )
-  }
+    );
+  };
   return (
     <div className="flex flex-col w-[100%] min-h-[100vh] h-max text-center items-center md:px-5 px-3 mb-16 pt-24 ">
       <div className="w-[95%] flex h-[3rem] ">
@@ -268,8 +268,9 @@ const GallerysubslugSlug = ({ imageNames }) => {
           viewBox="0 0 512 512"
           className="cursor-pointer rounded-full hover:scale-110 duration-300"
           onClick={() => {
-            router.push(`/gallery/${slug}`)
-          }}>
+            router.push(`/gallery/${slug}`);
+          }}
+        >
           <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" />
         </svg>
       </div>
@@ -280,7 +281,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
           whileInView="visible"
           transition={{ delay: 0.1, duration: 0.4 }}
           viewport={{ once: true }}
-          className={`font-extrabold text-2xl md:text-4xl lg:text-5xl ${poppins.className}  text-center text-[black] border-[#1f1b4e] border-b-2 mb-8 mx-auto`}>
+          className={`font-extrabold text-2xl md:text-4xl lg:text-5xl ${poppins.className}  text-center text-[black] border-[#1f1b4e] border-b-2 mb-8 mx-auto`}
+        >
           <span className="text-[#1f1b4e]">{subslug}</span>
         </motion.h3>
       </div>
@@ -303,7 +305,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
           loader.map((item, i) => (
             <div
               key={i}
-              className="w-[100%] md:w-[45%] lg:w-[23%] h-[20rem] mb-8 shadow-2xl rounded-lg ">
+              className="w-[100%] md:w-[45%] lg:w-[23%] h-[20rem] mb-8 shadow-2xl rounded-lg "
+            >
               <Skeleton className="h-full" />
             </div>
           ))
@@ -315,7 +318,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
                   <motion.div
                     layoutId={image}
                     key={i}
-                    className="h-max w-[100%] md:w-[45%] lg:w-[25%] mb-8 rounded-lg">
+                    className="h-max w-[100%] md:w-[45%] lg:w-[25%] mb-8 rounded-lg"
+                  >
                     <Image
                       src={`/galleries/${slug}/${subslug}/${image}`}
                       alt="image"
@@ -323,14 +327,14 @@ const GallerysubslugSlug = ({ imageNames }) => {
                       height={10}
                       className="lg:h-[20rem] w-full object-center lg:w-[95%] mx-auto object-cover rounded-lg cursor-pointer hover:scale-95 duration-300 "
                       onClick={() => {
-                        setSelectedId(image)
-                        showImagePopup(i)
+                        setSelectedId(image);
+                        showImagePopup(i);
                       }}
                       unoptimized
                       priority
                     />
                   </motion.div>
-                )
+                );
               })}
           </div>
         )}
@@ -341,7 +345,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
           <div className="relative w-[100vw] ">
             <button
               className="absolute right-10 bg-white p-2 rounded-full"
-              onClick={hideImagePopup}>
+              onClick={hideImagePopup}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -351,7 +356,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
                 stroke="currentColor"
                 strokeWidth="2"
                 strokeLinecap="round"
-                strokeLinejoin="round">
+                strokeLinejoin="round"
+              >
                 <line x1="18" y1="6" x2="6" y2="18"></line>
                 <line x1="6" y1="6" x2="18" y2="18"></line>
               </svg>
@@ -360,8 +366,9 @@ const GallerysubslugSlug = ({ imageNames }) => {
           <button
             className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white p-2 rounded-full"
             onClick={() => {
-              showNextImage()
-            }}>
+              showNextImage();
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -371,7 +378,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
               stroke="currentColor"
               strokeWidth="2"
               strokeLinecap="round"
-              strokeLinejoin="round">
+              strokeLinejoin="round"
+            >
               <line x1="5" y1="12" x2="19" y2="12"></line>
               <polyline points="12 5 19 12 12 19"></polyline>
             </svg>
@@ -379,8 +387,9 @@ const GallerysubslugSlug = ({ imageNames }) => {
           <button
             className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white p-2 rounded-full"
             onClick={() => {
-              showPrevImage()
-            }}>
+              showPrevImage();
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -390,7 +399,8 @@ const GallerysubslugSlug = ({ imageNames }) => {
               stroke="currentColor"
               strokeWidth="2"
               strokeLinecap="round"
-              strokeLinejoin="round">
+              strokeLinejoin="round"
+            >
               <line x1="19" y1="12" x2="5" y2="12"></line>
               <polyline points="12 19 5 12 12 5"></polyline>
             </svg>
@@ -416,27 +426,68 @@ const GallerysubslugSlug = ({ imageNames }) => {
         </motion.div>
       )}
     </div>
-  )
+  );
+};
+
+export default GallerysubslugSlug;
+
+// export async function getServerSideProps({ params }) {
+//   // console.log(params);
+//   const publicPath = path.join(
+//     process.cwd(),
+//     "public",
+//     "galleries",
+//     params.slug,
+//     params.subslug
+//   )
+
+//   // Read the contents of the public folder dynamically
+//   const imageNames = fs.readdirSync(publicPath)
+
+//   return {
+//     props: {
+//       imageNames,
+//     },
+//   }
+// }
+
+// Function to get possible paths
+export async function getStaticPaths() {
+  const galleryBasePath = path.join(process.cwd(), 'public', 'galleries');
+  const galleryFolders = fs.readdirSync(galleryBasePath);
+
+  const paths = galleryFolders.reduce((acc, gallery) => {
+    const subFolders = fs.readdirSync(path.join(galleryBasePath, gallery));
+    const subPaths = subFolders.map((subFolder) => ({
+      params: {
+        slug: gallery,
+        subslug: subFolder,
+      },
+    }));
+    return [...acc, ...subPaths];
+  }, []);
+
+  return {
+    paths,
+    fallback: false, // Or 'blocking' if you want to use Incremental Static Regeneration
+  };
 }
 
-export default GallerysubslugSlug
-
-export async function getServerSideProps({ params }) {
-  // console.log(params);
+// Function to get data for each path
+export async function getStaticProps({ params }) {
   const publicPath = path.join(
     process.cwd(),
-    "public",
-    "galleries",
+    'public',
+    'galleries',
     params.slug,
     params.subslug
-  )
+  );
 
-  // Read the contents of the public folder dynamically
-  const imageNames = fs.readdirSync(publicPath)
+  const imageNames = fs.readdirSync(publicPath);
 
   return {
     props: {
       imageNames,
     },
-  }
+  };
 }
