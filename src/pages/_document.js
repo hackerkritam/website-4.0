20a6242f8b1dbed9a421885f05437e83de58f713
Kritam Bhattarai <kritambhattarai4@gmail.com>
2025-01-07 00:48:40 +0545

    heading

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Head from 'next/head';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <title>Delta 5.0 - Purwanchal Campus</title>
+        <meta name="description" content="Reflecting on Delta's remarkable journey and substantial growth. A pivotal platform for leadership and positive impact in the engineering community." />
+      
+      </Head>
 
       <body>
         <Main />
